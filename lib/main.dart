ba9523834ebb38b9378e1b679d537ae21124c446
adarshrana205 <adarshrana205@gmail.com>
2019-03-22 14:45:54 +0530

    added code

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+import 'package:flutter_app/home_page.dart';
+
+void main() => runApp(new MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: "Calculator App",
+      theme: new ThemeData(primarySwatch: Colors.red),
+      home: new HomePage(),
+    );
+  }
+}
